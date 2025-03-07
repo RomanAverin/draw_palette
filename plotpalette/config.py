027c685156d2ffc8d70ad317728cd5e66f1203ca
Roman Averin <mail@romanaverin.com>
2025-03-07 11:02:33 +0300

    Changed default settings

--- plotpalette/config.py
+++ plotpalette/config.py
@@ -6,9 +6,9 @@ DEFAULT_CONFIG = {
     "spacing": 0.05,
     "dpi": 300,  # dpi of output image
     "rounding": 0.05,  # rounding of cell
-    "max_column_num": 8,  # max colomns
-    "cell_size": 2.0,
+    "max_column_num": 6,  # max colomns
+    "cell_size": 3.0,
     "fontfamily": "monospace",
-    "color_name_size": 25,  # font size of label color name
-    "color_hex_size": 20,  # font size of label color HEX value
+    "color_name_size": 35,  # font size of label color name
+    "color_hex_size": 25,  # font size of label color HEX value
 }
